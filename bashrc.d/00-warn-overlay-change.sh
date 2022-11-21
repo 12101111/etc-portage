6b432c25d2c281f761fd308dbf552bc677c5e523
12101111 <w12101111@gmail.com>
2022-11-21 21:39:58 +0800

    sync

--- bashrc.d/00-warn-overlay-change.sh
+++ bashrc.d/00-warn-overlay-change.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 warn_overlay_change_pretend() {
-	overlay_from=$(cat "${EROOT}/var/db/pkg/${CATEGORY}/${PN}-${REPLACING_VERSIONS}/repository")
-	if [[ "${overlay_from}" != "${PORTAGE_REPO_NAME}" ]]; then
-		ewarn "${CATEGORY}/${PN} used to be installed from ::${overlay_from},"
-		ewarn "but is now going to be installed from ::${PORTAGE_REPO_NAME} now!"
+	if [[ ! -z "${REPLACING_VERSIONS}" ]]; then
+		overlay_from=$(cat "${EROOT}/var/db/pkg/${CATEGORY}/${PN}-${REPLACING_VERSIONS}/repository")
+		if [[ "${overlay_from}" != "${PORTAGE_REPO_NAME}" ]]; then
+			ewarn "${CATEGORY}/${PN} used to be installed from ::${overlay_from},"
+			ewarn "but is now going to be installed from ::${PORTAGE_REPO_NAME} now!"
+		fi
 	fi
 }
 
 warn_overlay_change_preinst() {
-	overlay_from=$(cat "${EROOT}/var/db/pkg/${CATEGORY}/${PN}-${REPLACING_VERSIONS}/repository")
-	if [[ "${overlay_from}" != "${PORTAGE_REPO_NAME}" ]] &&
-		[[ "${overlay_from}" == "12101111-overlay" ]] &&
-		[[ -z ${I_KNOW_WHAT_I_AM_DOING} ]]; then
-		die "Don't break your system"
+	if [[ ! -z "${REPLACING_VERSIONS}" ]]; then
+		overlay_from=$(cat "${EROOT}/var/db/pkg/${CATEGORY}/${PN}-${REPLACING_VERSIONS}/repository")
+		if [[ "${overlay_from}" != "${PORTAGE_REPO_NAME}" ]] &&
+			[[ "${overlay_from}" == "12101111-overlay" ]] &&
+			[[ -z ${I_KNOW_WHAT_I_AM_DOING} ]]; then
+			die "Don't break your system"
+		fi
 	fi
 }
 
